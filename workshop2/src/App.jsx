6b184ebc1c97922d43b03d1573e0d0c2423455c3
PMorganGit <morganpouilly@gmail.com>
2024-05-13 10:09:50 +0200

    commit

--- workshop2/src/App.jsx
+++ workshop2/src/App.jsx
@@ -1,11 +1,14 @@
+import Biography from './components/biography/biography'
 import './App.css'
+
+function App() {
 
 
   return (
-    <>
-
-    </>
+    <div>
+    <Biography />
+    </div>
   )
-
+}
 
 export default App
---
+++ workshop2/src/components/biography/biography.jsx
@@ -0,0 +1,19 @@
+function Biography() {
+
+  return (
+    <>
+    <div className="title-info">
+        <div className="title_lakers">
+        <h2>Les Lakers</h2>
+        </div>
+        <div>
+            <p>
+                oisjdoisjfoisdjfosidjf
+            </p>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Biography
